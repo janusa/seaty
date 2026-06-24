87255493cc7c7b0ce6638b83349f55e135115290
Janusa Ragunathan <11028599+janusa@users.noreply.github.com>
2026-06-24 18:56:34 +0200

    #1 Add files created by spring initializr

---
+++ src/main/kotlin/com/janusa/seaty/SeatyApplication.kt
@@ -0,0 +1,11 @@
+package com.janusa.seaty
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeatyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SeatyApplication>(*args)
+}
---
+++ src/test/kotlin/com/janusa/seaty/SeatyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.janusa.seaty
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeatyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
